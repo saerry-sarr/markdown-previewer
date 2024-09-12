de7c6be2a07bd2d3544ed50944ad5bb4dc2c33cd
saerrysarr <sara.schuenemann@gmx.de>
2024-09-12 12:46:07 +0200

    Add form and basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'markdown-previewer';
+  public title = 'markdown-previewer';
+  public form: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.form = this.fb.group({
+      editing: '',
+      preview: ''
+    })
+  }
 }
