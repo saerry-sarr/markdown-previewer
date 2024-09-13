021d2cf8faaa5da9ea011c1aa3a747053444f08e
saerrysarr <sara.schuenemann@gmx.de>
2024-09-13 11:50:21 +0200

    Add markdown parser and logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, SecurityContext } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { md } from './app.config';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  public title = 'markdown-previewer';
+  public title = 'simple-markdown-previewer';
   public form: FormGroup;
+  public preview: string | null = '';
 
-  constructor(private readonly fb: FormBuilder) {
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly sanitizer: DomSanitizer
+  ) {
     this.form = this.fb.group({
       editing: '',
-      preview: ''
-    })
+    });
+
+    md.linkify.set({ fuzzyEmail: false });
+
+    this.form.get('editing')?.valueChanges.subscribe((input: string) => {
+      this.preview = this.sanitizer.sanitize(
+        SecurityContext.HTML,
+        md.render(input)
+      );
+    });
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,29 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import highlightJs from 'highlight.js';
+import markdownit from 'markdown-it';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
+
+export const md: markdownit = markdownit({
+  html: true,
+  linkify: true,
+  typographer: true,
+  highlight: highlightCodeBlock,
+});
+
+export function highlightCodeBlock(str: string, lang: string) {
+  if (lang && highlightJs.getLanguage(lang)) {
+    try {
+      return highlightJs.highlight(str, { language: lang }).value;
+    } catch (__) {}
+  }
+
+  return '';
+}
